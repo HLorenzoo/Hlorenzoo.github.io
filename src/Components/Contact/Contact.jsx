6add247651bd8d73733f029b0a2620d3e2a03b1d
Hlorenzoo <hernanlorenzo97@hotmail.com>
2022-10-12 14:55:54 -0300

    contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,10 @@
-import { Divider, Flex } from "@chakra-ui/react";
+import { Divider, Flex, Text } from "@chakra-ui/react";
 import React from "react";
+import useAppContext from "../../context/context";
 import TitleSection from "../AboutMe/TitleSection";
 
 const Contact = ({ data }) => {
+  const { language, setlanguage } = useAppContext();
   return (
     <Flex
       alignItems="center"
@@ -14,6 +16,16 @@ const Contact = ({ data }) => {
       mb="45px"
     >
       <TitleSection data={data} n={4} />
+      <Flex flexDirection="column" alignItems="center" justifyContent="center">
+        <Text className="sf" fontSize="4rem" color="#19b1ff">
+          {language === "es" ? "Contactame" : "Get In Touch"}
+        </Text>
+        <Text color="white" className="calibre">
+          {language === "es"
+            ? "Estoy "
+            : "I'm happy to answer any questions you may have"}
+        </Text>
+      </Flex>
     </Flex>
   );
 };
